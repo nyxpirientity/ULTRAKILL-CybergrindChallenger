daacbdaf40f387a53ba3ed5db527c724f174cf0f
Nixya <64989593+nyxpirientity@users.noreply.github.com>
2026-03-16 13:16:13 -0700

    Initial Commit

---
+++ Source/CybergrindChallenger.cs
@@ -0,0 +1,274 @@
+﻿using UnityEngine;
+using BepInEx;
+using System.Collections.Generic;
+using Nyxpiri.ULTRAKILL.NyxLib;
+using ULTRAKILL.Cheats;
+using System;
+using System.IO;
+
+namespace Nyxpiri.ULTRAKILL.CybergrindChallenger
+{
+    public static class Cheats
+    {
+        public const string CybergrindChallenger = "nyxpiri.cybergrind-challenger";
+    }
+    
+    [BepInPlugin("com.nyxpiri.bepinex.plugins.ultrakill.cybergrind-challenger", "Cybergrind Challenger", "0.0.0.1")]
+    [BepInProcess("ULTRAKILL.exe")]
+    public class CybergrindChallenger : BaseUnityPlugin
+    {
+        protected void Awake()
+        {
+            Log.Initialize(Logger);
+            Options.Initialize(Config);
+            Cybergrind.PreCybergrindBegin += OnCybergrindBegin;
+            Cybergrind.PostCybergrindNextWave += OnNextWaveBegin;
+            NyxLib.Cheats.ReadyForCheatRegistration += RegisterCheats;
+        }
+
+        private void RegisterCheats(CheatsManager cheatsManager)
+        {
+            cheatsManager.RegisterCheat(new ToggleCheat(
+                "Cybergrind Challenger", 
+                Cheats.CybergrindChallenger,
+                onDisable: (cheat) =>
+                {
+                },
+                onEnable: (cheat, manager) =>
+                {
+                    if (Cybergrind.IsActive)
+                    {
+                        RefreshChallenges();
+                    }
+                }
+            ), "CYBERGRIND");
+        }
+
+        protected void Start()
+        {
+        }
+
+        protected void Update()
+        {
+
+        }
+
+        protected void LateUpdate()
+        {
+
+        }
+
+        private void OnCybergrindBegin(EventMethodCanceler cancelInfo, EndlessGrid endlessGrid)
+        {
+            if (NyxLib.Cheats.IsCheatDisabled(Cheats.CybergrindChallenger))
+            {
+                return;
+            }
+
+            RefreshChallenges();
+        }
+
+        private void RefreshChallenges()
+        {
+            IntenseChallenges.Clear();
+            Challenges.Clear();
+
+            try
+            {
+                Options.Load();
+            }
+            catch (System.Exception e)
+            {
+                if (e is InvalidDataException)
+                {
+                    Log.Error($"Failed to load options. :c Here's info maybe probably '{e.Message}'");
+                    QuickMsgPool.DisplayQuickMsg("CYBERGRINDCHALLENGER FAILED TO LOAD OPTIONS :c", Color.red, 8.0f, velocity: Vector3.down * 400.0f, 42.0f, false);
+                    QuickMsgPool.DisplayQuickMsg($"{e.Message}", Color.red, 8.0f, velocity: Vector3.down * 560.0f, 16.0f, false);
+                    return;
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            var challengesConfigs = Options.Challenges;
+
+            var intenseChallengesNames = Options.IntenseChallenges;
+            var standardChallengesNames = Options.StandardChallenges;
+
+            bool hitProblems = false;
+
+            challengeIdx = 0;
+            intenseChallengeIdx = 0;
+            CheatsEnabledByUs = new string[0];
+
+            FieldPublisher<CheatsManager, Dictionary<string, ICheat>> idToCheat = new FieldPublisher<CheatsManager, Dictionary<string, ICheat>>(NyxLib.Cheats.Manager, "idToCheat");
+
+            foreach (var challenge in challengesConfigs)
+            {
+                Log.Debug($"Challenge exists by named {challenge.Key}");
+            }
+
+            foreach (var challengeName in intenseChallengesNames)
+            {
+                if (!challengesConfigs.TryGetValue(challengeName, out var challengeCheats))
+                {
+                    Log.Warning($"Failed to find challenge in intense challenge list, named {challengeName}");
+                    hitProblems = true;
+                    continue;
+                }
+
+                bool nonExistentCheatFound = false;
+                foreach (var cheatName in challengeCheats)
+                {
+                    if (!idToCheat.Value.ContainsKey(cheatName))
+                    {
+                        Log.Warning($"Failed to find cheat by name {cheatName}, thus canceling intense challenge {challengeName}! (You may need a mod for the cheat? Including default included cheats!)");
+                        hitProblems = true;
+                        nonExistentCheatFound = true;
+                        break;
+                    }
+                }
+
+                if (nonExistentCheatFound)
+                {
+                    continue;
+                }
+
+                Log.Debug($"Adding intense challenge by name {challengeName}");
+                IntenseChallenges.Add((challengeName, challengeCheats.ToArray()));
+            }
+
+            foreach (var challengeName in standardChallengesNames)
+            {
+                if (!challengesConfigs.TryGetValue(challengeName, out var challengeCheats))
+                {
+                    Log.Warning($"Failed to find challenge in standard challenge list, named {challengeName}");
+                    hitProblems = true;
+                    continue;
+                }
+                
+                bool nonExistentCheatFound = false;
+                foreach (var cheatName in challengeCheats)
+                {
+                    if (!idToCheat.Value.ContainsKey(cheatName))
+                    {
+                        Log.Warning($"Failed to find cheat by name {cheatName}, thus canceling standard challenge {challengeName}! (You may need a mod for the cheat? Including default included cheats!)");
+                        hitProblems = true;
+                        nonExistentCheatFound = true;
+                        break;
+                    }
+                }
+
+                if (nonExistentCheatFound)
+                {
+                    continue;
+                }
+
+                Log.Debug($"Adding challenge by name {challengeName}");
+                Challenges.Add((challengeName, challengeCheats.ToArray()));
+            }
+
+            if (hitProblems)
+            {
+                QuickMsgPool.DisplayQuickMsg("CybergrindChallenger ran into some problems!", Color.yellow, 8.0f, velocity: Vector3.down * 400.0f, 42.0f, false);
+                QuickMsgPool.DisplayQuickMsg("Logs should have more info, so long as BepInEx is configured to display the right log types!", Color.yellow, 8.0f, velocity: Vector3.down * 560.0f, 16.0f, false);
+            }
+        }
+
+        private void OnNextWaveBegin(EventMethodCancelInfo cancelInfo, EndlessGrid eg)
+        {
+            for (int i = 0; i < CheatsEnabledByUs.Length; i++)
+            {
+                NyxLib.Cheats.Manager.DisableCheat(CheatsEnabledByUs[i]);
+            }
+            
+            CheatsEnabledByUs = new string[0];
+
+            if (NyxLib.Cheats.IsCheatDisabled(Cheats.CybergrindChallenger))
+            {
+                return;
+            }
+
+            if (NyxLib.Cheats.Enabled)
+            {
+                NyxLib.Cheats.Manager.ToggleCheat(NyxLib.Cheats.Manager.GetCheatInstance<KillAllEnemies>());
+            }
+
+            bool useIntenseChallenges = (((eg.currentWave % 5) == 0 || (eg.currentWave % 5) == 2) && eg.currentWave != eg.startWave) && IntenseChallenges.Count > 0;
+
+            if (Challenges.Count == 0 && IntenseChallenges.Count == 0)
+            {
+
+                QuickMsgPool.DisplayQuickMsg($"WE HAVE NO USABLE CHALLENGES? :c", new Color(0.9f, 0.9f, 0.6f), 8.0f, velocity: Vector3.down * 200.0f, 42.0f);
+                return;
+            }
+
+            List<(string, string[])> challengePool = useIntenseChallenges ? IntenseChallenges : Challenges;
+            
+            ref int currentChallengeIdx = ref (useIntenseChallenges ? ref challengeIdx : ref intenseChallengeIdx);
+
+            if (currentChallengeIdx == 0)
+            {
+                if (useIntenseChallenges)
+                {
+                    for (int i = 0; i < IntenseChallenges.Count; i++)
+                    {
+                        int targetIdx = UnityEngine.Random.Range(0, IntenseChallenges.Count - 1);
+                        var movingValue = IntenseChallenges[i];
+                        IntenseChallenges.RemoveAt(i);
+                        IntenseChallenges.Insert(targetIdx, movingValue);
+                    }
+                }
+                else
+                {
+                    for (int i = 0; i < Challenges.Count; i++)
+                    {
+                        int targetIdx = UnityEngine.Random.Range(0, Challenges.Count - 1);
+                        var movingValue = Challenges[i];
+                        Challenges.RemoveAt(i);
+                        Challenges.Insert(targetIdx, movingValue);
+                    }
+                }
+            }
+            
+            var challenge = challengePool[currentChallengeIdx];
+            currentChallengeIdx = (currentChallengeIdx + 1) % (useIntenseChallenges ? IntenseChallenges.Count : Challenges.Count);
+            
+            FieldPublisher<CheatsManager, Dictionary<string, ICheat>> idToCheat = new FieldPublisher<CheatsManager, Dictionary<string, ICheat>>(NyxLib.Cheats.Manager, "idToCheat");
+            CheatsEnabledByUs = challenge.Item2;
+
+            float downwardOffsetBase = 0.0f;
+
+            if (useIntenseChallenges)
+            {
+                QuickMsgPool.DisplayQuickMsg($"INTENSITY SPIKE", new Color(1.0f, 0.1f, 0.1f), 5.0f, velocity: Vector3.down * 200.0f, 42.0f);
+                QuickMsgPool.DisplayQuickMsg($"{challenge.Item1}", new Color(1.0f, 0.3f, 0.3f), 5.0f, velocity: Vector3.down * 320.0f, 36.0f);
+                downwardOffsetBase = 500.0f;
+            }
+            else
+            {
+                QuickMsgPool.DisplayQuickMsg($"{challenge.Item1}", new Color(1.0f, 0.4f, 0.4f), 5.0f, velocity: Vector3.down * 220.0f, 36.0f);
+                downwardOffsetBase = 350.0f;
+            }
+
+            for (int i = 0; i < challenge.Item2.Length; i++)
+            {
+                string cheatName = challenge.Item2[i];
+                var cheat = idToCheat.Value[cheatName];
+                
+                QuickMsgPool.DisplayQuickMsg($"+ {cheat.LongName.ToUpper()}", new Color(0.875f, 0.75f, 1.0f), 5.0f, velocity: Vector3.down * ((float)((i) * 90.0f) + downwardOffsetBase), 24.0f);
+                cheat.Enable(NyxLib.Cheats.Manager);
+            }
+            
+            NyxLib.Cheats.Manager.RefreshCheatStates();
+        }
+
+        private int intenseChallengeIdx = 0;
+        private int challengeIdx = 0;
+        private string[] CheatsEnabledByUs = new string[0];
+        private List<(string, string[])> Challenges = new List<(string, string[])>();
+        private List<(string, string[])> IntenseChallenges = new List<(string, string[])>();
+    }
+}
---
+++ Source/Options.cs
@@ -0,0 +1,207 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using BepInEx.Configuration;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using UnityEngine.Assertions;
+
+namespace Nyxpiri.ULTRAKILL.CybergrindChallenger
+{
+    public static class Options
+    {
+        public static Dictionary<string, List<string>> Challenges = null;
+        public static List<string> StandardChallenges = null;
+        public static List<string> IntenseChallenges = null;
+        public static string ChallengesConfigPath = null;
+
+        internal static void Initialize(ConfigFile config)
+        {
+            Assert.IsNotNull(config);
+ 
+            Config = config;
+
+            ChallengesConfigPath = $"{BepInEx.Paths.ConfigPath}/com.nyxpiri.cybergrind-challenger.challenges-cfg.json";
+
+            Challenges = new Dictionary<string, List<string>>
+            {
+                {"SEEING DOUBLE", new List<string>{HeckPuppets, GiveEnemiesFriends}},
+                {"BRUTAL HECK", new List<string>{AggressiveAgony, HeatOfHeck}},
+                {"GET UP ON THEIR BACK", new List<string>{HydraMode, HeatOfHeck}},
+                {"MISCONFIGURED", new List<string>{SelfConscience, BadGyro}},
+                {"ULTRACARE", new List<string>{GiveEnemiesFriends, BloodFueledEnemies}},
+                {"NOW SWAP",new List<string>{NyxLib.Cheats.SandAllEnemiesID, BloodFueledEnemies}},
+                {"A LOOK IN THE MIRROR", new List<string>{ BloodFueledEnemies, FeedbackersForEveryone }},
+                {"EVERYONE'S A BOOSTER", new List<string>{ AggressiveAgony, FeedbackersForEveryone }},
+                {"BAD GAME DESIGN", new List<string>{ BadGyro, MundaneMurder }},
+                {"PAINFULLY SALTY", new List<string>{ SaltyEnemies, AggressiveAgony }},
+                {"AA FORCERAD", new List<string>{ AggressiveAgony, NyxLib.Cheats.RadiantAllEnemies }},
+
+                {"SSADISTIC HECK", new List<string>{ SaltyEnemies, SelfConscience, HeatOfHeck, AggressiveAgony }},
+                {"HECK SPECIAL", new List<string>{ SaltyEnemies, HeckPuppets, HeatOfHeck, AggressiveAgony }},
+                {"STYLE ISSUE", new List<string>{ SaltyEnemies, SelfConscience, HeatOfHeck, HeckPuppets }},
+                {"HEAT OF GREED", new List<string>{ NyxLib.Cheats.SandAllEnemiesID, BloodFueledEnemies, HeatOfHeck }},
+                {"TECH ISSUES", new List<string>{ BadGyro, HeatOfHeck, SelfConscience }},
+                {"ULTRASWAP",new List<string>{NyxLib.Cheats.SandAllEnemiesID, BloodFueledEnemies, FeedbackersForEveryone}},
+                {"COUNTER-COUNTER-ER", new List<string>{ AggressiveAgony, FeedbackersForEveryone, HeatOfHeck }},
+                {"FURIOUS MITOSIS", new List<string>{ SaltyEnemies, HeatOfHeck, HydraMode }}
+            };
+
+            StandardChallenges = new List<string>
+            {
+                "BRUTAL HECK", "MISCONFIGURED", "ULTRACARE", "NOW SWAP", "BAD GAME DESIGN", "PAINFULLY SALTY", "AA FORCERAD", "A LOOK IN THE MIRROR", "EVERYONE'S A BOOSTER",
+            };
+
+            IntenseChallenges = new List<string>
+            {
+                "SSADISTIC HECK", "HECK SPECIAL", "STYLE ISSUE", "HEAT OF GREED", "TECH ISSUES", "COUNTER-COUNTER-ER", "ULTRASWAP",
+            };
+
+            if (!File.Exists(ChallengesConfigPath))
+            {
+                SaveChallengesConfig();
+            }
+            else
+            {
+                try
+                {
+                    LoadChallengesConfig();                    
+                }
+                catch (System.Exception e)
+                {
+                    if (e is InvalidDataException)
+                    {
+                        Log.Error($"Failed to read challenges config due to InvalidDataException... {e}");
+                    }
+                }
+            }
+        }
+
+        private static void LoadChallengesConfig()
+        {
+            string jsonString = File.ReadAllText(ChallengesConfigPath);
+            var data = JsonConvert.DeserializeObject<JObject>(jsonString) ?? throw new InvalidDataException();
+            var challengesGeneric = data.GetValue("Challenges");
+
+            if (challengesGeneric == null)
+            {
+                throw new InvalidDataException("Challenges was not present");
+            }
+
+            if (challengesGeneric.Type != JTokenType.Object)
+            {
+                throw new InvalidDataException("Challenges was not an JsonObject");
+            }
+
+            var jChallenges = challengesGeneric as JObject;
+            var challenges = new Dictionary<string, List<string>>();
+
+            Assert.IsNotNull(jChallenges);
+
+            foreach (var item in jChallenges)
+            {
+                var cheatsGeneric = item.Value;
+
+                if (cheatsGeneric.Type != JTokenType.Array)
+                {
+                    continue;
+                }
+
+                var jCheats = cheatsGeneric as JArray;
+
+                Assert.IsNotNull(cheatsGeneric);
+
+                List<string> cheats = new List<string>(jCheats.Count);
+
+                foreach (var cheat in jCheats)
+                {
+                    if (cheat.Type != JTokenType.String)
+                    {
+                        continue;
+                    }
+
+                    cheats.Add(cheat.ToString());
+                }
+
+                challenges.Add(item.Key, cheats);
+            }
+
+            List<string> standardChallenges = new List<string>();
+            ExtractChallengesDataFromJArray(data, challenges, standardChallenges, "StandardChallenges");
+
+            List<string> intenseChallenges = new List<string>();
+            ExtractChallengesDataFromJArray(data, challenges, intenseChallenges, "IntenseChallenges");
+
+            Challenges = challenges;
+            StandardChallenges = standardChallenges;
+            IntenseChallenges = intenseChallenges;
+        }
+
+        private static void ExtractChallengesDataFromJArray(JObject data, Dictionary<string, List<string>> challenges, List<string> challengeNames, string jArrayName)
+        {
+            var challengeNamesGeneric = data.GetValue(jArrayName);
+
+            if (challengeNamesGeneric == null)
+            {
+                throw new InvalidDataException($"{jArrayName} did not exist, which is invalid");
+            }
+
+            if (challengeNamesGeneric.Type != JTokenType.Array)
+            {
+                throw new InvalidDataException($"{jArrayName} was not an array of strings, which is invalid");
+            }
+
+            var jChallengeNames = challengeNamesGeneric as JArray;
+            foreach (var challengeGeneric in jChallengeNames)
+            {
+                if (challengeGeneric.Type != JTokenType.String)
+                {
+                    throw new InvalidDataException($"{jArrayName} contained a token which wasn't a string, which is invalid");
+                }
+
+                string challengeName = challengeGeneric.ToString();
+
+                if (!challenges.ContainsKey(challengeName))
+                {
+                    throw new InvalidDataException($"{jArrayName} contained challenge by name {challengeName} which did not seem to exist");
+                }
+
+                challengeNames.Add(challengeName);
+            }
+        }
+
+        private static void SaveChallengesConfig()
+        {
+            Dictionary<string, object> JsonData = new Dictionary<string, object>
+            {
+                {"Challenges", Challenges},
+                {"StandardChallenges", StandardChallenges},
+                {"IntenseChallenges", IntenseChallenges},
+            };
+
+            string jsonString = JsonConvert.SerializeObject(JsonData, Formatting.Indented, new JsonSerializerSettings { });
+
+            File.WriteAllText(ChallengesConfigPath, jsonString);
+        }
+
+        internal static void Load()
+        {
+            LoadChallengesConfig();
+        }
+
+        const string BadGyro = "nyxpiri.bad-gyro";
+        const string AggressiveAgony = "nyxpiri.aggressive-agony";
+        const string HeatOfHeck = "nyxpiri.heat-of-heck";
+        const string GiveEnemiesFriends = "nyxpiri.give-enemies-friends";
+        const string BloodFueledEnemies = "nyxpiri.blood-fueled-enemies";
+        const string SelfConscience = "nyxpiri.self-conscience";
+        const string MundaneMurder = "nyxpiri.mundane-murder";
+        const string SaltyEnemies = "nyxpiri.salty-enemies";
+        const string HeckPuppets = "nyxpiri.heck-puppets";
+        const string HydraMode = "nyxpiri.hydra-mode";
+        const string FeedbackersForEveryone = "nyxpiri.feedbackers-for-everyone";
+
+        private static ConfigFile Config = null;
+    }
+}
